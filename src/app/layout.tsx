f05455d241888fc999e7493461becb9453473748
a3magic3pocket <a3magic3pocket@gmail.com>
2024-02-21 23:24:39 +0900

    Design the main page roughly.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import "@fortawesome/fontawesome-svg-core/styles.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="flex justify-center items-center">
+          <div className="flex main-content-wrap bg-black border border-black border-solid">
+            {children}
+          </div>
+        </div>
+        {/* 430 x 932 */}
+      </body>
     </html>
   );
 }
