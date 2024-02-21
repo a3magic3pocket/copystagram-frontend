cb2bbcc6b29291100ea4ec4704b737421625b18d
a3magic3pocket <a3magic3pocket@gmail.com>
2024-02-22 08:41:24 +0900

    Design the main page roughly.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,20 @@
-import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Post from "@/component/Post";
+import LikeIcon from "@/component/icon/Like";
+import MessageIcon from "@/component/icon/Message";
 
 export default function Page() {
-  return <div>main</div>;
+  return (
+    <div className="flex flex-col w-full">
+      <div className="flex flex-row w-full h-full justify-between align-items">
+        <div className="flex font-aAdulsaScript text-4xl pl-2">Copystagram</div>
+        <div className="flex flex-row pr-2">
+          <LikeIcon className="flex self-center p-2" />
+          <MessageIcon className="flex self-center p-2" />
+        </div>
+      </div>
+      {[...Array(3)].map((_, i) => {
+        return <Post key={i} />;
+      })}
+    </div>
+  );
 }
