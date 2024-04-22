5a1e05f3b398b4f1d4bce7203529026a533213ae
a3magic3pocket <a3magic3pocket@gmail.com>
2024-04-22 12:45:47 +0900

    Set the priority of loading.svg to true.

--- src/component/loading/BaseLoading.tsx
+++ src/component/loading/BaseLoading.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function BaseLoading() {
   return (
     <div className="flex animate-spin h-10 w-10">
-      <Image src="/image/loading.svg" alt="daf" width={50} height={50} />
+      <Image src="/image/loading.svg" alt="daf" width={50} height={50} priority={true} />
     </div>
   );
 }
